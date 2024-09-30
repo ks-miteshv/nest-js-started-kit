fafd508953de6d6319bf06d8a0e45edae619ca07
ks-miteshv <mitesh.viras@kalkani.in>
2024-09-30 11:14:25 +0530

    mongodb connection string added.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import * as Joi from 'joi';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_CONNECTION_STRING'),
+      }),
+    }),
     ConfigModule.forRoot({
       validationSchema: Joi.object({
         JWT_SECRET: Joi.string().required(),
         HOST: Joi.string().optional(),
         PORT: Joi.number().optional(),
+        MONGO_CONNECTION_STRING: Joi.string().required(),
       }),
     }),
   ],
